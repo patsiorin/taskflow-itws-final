54ae918b2007ae8593735b2f07c527b6a65a883c
Patsiorin <patsiorin@menfin.ru>
2026-06-04 10:58:04 +0200

    chore: print dev server urls

---
+++ scripts/print-dev-urls.mjs
@@ -0,0 +1,11 @@
+const frontendUrl = 'http://127.0.0.1:5173';
+const backendUrl = 'http://127.0.0.1:3000/api';
+
+setTimeout(() => {
+  console.log('');
+  console.log('TaskFlow dev servers');
+  console.log(`Frontend: ${frontendUrl}`);
+  console.log(`Backend:  ${backendUrl}`);
+  console.log('');
+}, 2500);
+
